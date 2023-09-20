a1147f844edaac833c9fe101fe9b1217d7400dc1
Ryder Paredes <ryderworks25@gmail.com>
2023-09-20 16:39:33 -0400

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-gray-100 h-full">
+    <header class="bg-white shadow h-16">
+      <router-link :to="{name: 'home'}">
+        Home
+      </router-link>
+    </header>
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
