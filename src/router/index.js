975e5248812b5914504d164c87b1791ae7b7198b
Ryder Paredes <ryderworks25@gmail.com>
2023-09-21 10:52:59 -0400

    Second Commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue'
-import MealList from '../views/MealList.vue'
+import MealsByIngredient from '../views/MealsByIngredient.vue'
+import MealsByLetter from '../views/MealsByLetter.vue'
+import MealsByName from '../views/MealsByName.vue'
 
 const routes = [
     {
@@ -9,9 +11,19 @@ const routes = [
         component: Home,
     },
     {
-        path: '/letter/:letter',
+        path: '/by-name/:name?',
+        name: "byName",
+        component: MealsByName,
+    },
+    {
+        path: '/by-letter/:letter?',
         name: "byLetter",
-        component: MealList,
+        component: MealsByLetter,
+    },
+    {
+        path: '/by-ingredient/:ingredient?',
+        name: "byIngredient",
+        component: MealsByIngredient,
     },
 ];
 
