6964b99a443a67fe0c9c64e3cc425435ea6766fb
Ryder Paredes <ryderworks25@gmail.com>
2023-09-22 20:32:09 -0400

    updated search meals tab

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,5 @@
 <template>
     <div class="flex flex-col p-8">
-        <input 
-        type="text" 
-        class="rounded border-2 border-gray-200 w-full" 
-        placeholder="Search for Meals" 
-        />
-        
         <div class="flex justify-center gap-2 mt-2">
             <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
                 {{ letter }}
